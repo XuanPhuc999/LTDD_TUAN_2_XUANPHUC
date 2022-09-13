9d1fb90d3cda44b8dd201caec0be47a9036fb967
XuanPhuc1820 <xp12071997@gmail.com>
2022-09-13 22:37:22 +0700

    first commit

---
+++ app/src/main/java/ltdd/tuan_1/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package ltdd.tuan_1.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
